2870ec62a4a76274e6a40629c4b26009c310715a
Smarita-New <140679579+Smarita-New@users.noreply.github.com>
2025-06-26 03:41:44 +0800

    Update init_alist.sh

--- alist-lib/scripts/init_alist.sh
+++ alist-lib/scripts/init_alist.sh
@@ -4,13 +4,8 @@ GIT_REPO="https://github.com/OpenListTeam/OpenList.git"
 TAG_NAME=$(git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --sort='version:refname' --tags $GIT_REPO | tail --lines=1 | cut --delimiter='/' --fields=3)
 
 echo "AList - ${TAG_NAME}"
-rm -rf ./src
 unset GIT_WORK_TREE
-git clone --branch "$TAG_NAME" https://github.com/OpenListTeam/OpenList.git ./src
-rm -rf ./src/.git
+git clone --branch "$TAG_NAME" https://github.com/OpenListTeam/OpenList.git
+rm -rf .git
 
-mv -f ./src/* ../
-rm -rf ./src
-
-cd ../
 go mod edit -replace github.com/djherbis/times@v1.6.0=github.com/jing332/times@latest
