38c0ab0300a325590c409f22afa19cefec5dd057
Smarita-New <140679579+Smarita-New@users.noreply.github.com>
2025-06-26 03:50:15 +0800

    Update init_web.sh

--- alist-lib/scripts/init_web.sh
+++ alist-lib/scripts/init_web.sh
@@ -3,6 +3,7 @@
 curl -L https://github.com/OpenListTeam/OpenList-Frontend/releases/latest/download/openlist-frontend-dist-v4.0.3.tar.gz  -o dist.tar.gz
 #tar -zxvf dist.tar.gz
 #rm -rf ../public/dist
+mv dist.tar.gz ../public/dist
 cd ../public/dist
 tar -zxvf dist.tar.gz
 #mv -f dist ../public
